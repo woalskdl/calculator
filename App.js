4a866d7bb89b8a74b0cf2fed6f9110af7ac1cb3b
jay nam <woalskdl@gmail.com>
2023-11-03 22:10:06 +0900

    project init, 변수 useState 작성

--- App.js
+++ App.js
@@ -1,7 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
+import { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+// project init >> npx create-expo-app calculator
+
 export default function App() {
+
+  const [input, setInput] = useState(0);
+  const [currentOperator, setCurrentOperator] = useState(null);
+  const [result, setResult] = useState(null);
+  const [tempInput, setTempInput] = useState(null);
+  const [tempOperator, setTempOperator] = useState(null);
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
